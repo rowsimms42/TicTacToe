7e7c8b7208c29a930a7b1cd2f67775a86eaf7e80
Rowan Simmons <59707215+rowsimms42@users.noreply.github.com>
2020-08-05 10:53:36 -0400

    updates

--- client_chat.py
+++ client_chat.py
@@ -1,24 +1,79 @@
+# https://medium.com/byte-tales/the-classic-tic-tac-toe-game-in-python-3-1427c68b8874
+
+import time
 from socket import *
 
 server_name = gethostname()
-port_number = 4247
+port_number = 4248
 total_length = 0
 # create client socket
-# sock_stream indicates TCP socket rather than UDP
 client_socket = socket(AF_INET, SOCK_STREAM)
 # establish connection between server and client
 client_socket.connect((server_name, port_number))
-print("Welcome. To exit chat, enter /q.")
+
+the_board = {'7': ' ', '8': ' ', '9': ' ',
+             '4': ' ', '5': ' ', '6': ' ',
+             '1': ' ', '2': ' ', '3': ' '}
+
+#  game board
+def print_board(b):
+    print(b['7'] + '|' + b['8'] + '|' + b['9'])
+    print('-+-+-')
+    print(b['4'] + '|' + b['5'] + '|' + b['6'])
+    print('-+-+-')
+    print(b['1'] + '|' + b['2'] + '|' + b['3'])
+
+
+def empty_board():
+    print("7" + '|' + "8" + '|' + "9")
+    print('-+-+-')
+    print("4" + '|' + "5" + '|' + "6")
+    print('-+-+-')
+    print("1" + '|' + "2" + '|' + "3")
+
+
+def play():
+    ex = False
+    while True:  # game loop
+        rec_msg = client_socket.recv(4026)
+        print(rec_msg.decode())
+        response = rec_msg[0]
+
+        if response == "#":
+            while True:  # input validation
+                req_msg = raw_input("> ")
+                if req_msg == "/q":
+                    ex = True
+                    break
+                elif req_msg not in {'1', '2', '3', '4', '5', '6', '7', '8', '9'} or the_board[req_msg] != ' ':
+                    print("Number must be between 1 and 9 and not already taken.")
+                else:
+                    req_msg = req_msg
+                    the_board[req_msg] = 'x'  # input player move
+                    print_board(the_board)
+                    print("")
+                    break
+            if ex:  # exit game
+                client_socket.send(req_msg.encode())
+                print("Exiting game...")
+                break
+            else:
+                client_socket.send(req_msg.encode())
+        elif response == "|":
+            num = str(rec_msg[1])
+            the_board[num] = 'o'  # input server move
+            print_board(the_board)
+            print("")
+        elif response == "*":  # print empty board at beginning to show key
+            empty_board()
+            print("")
+        elif response == "~":  # game over
+            break
+        else:
+            continue
+
 
 while True:
-    # output message
-    rec_msg = client_socket.recv(4026)
-    print(rec_msg.decode())
-    req_msg = raw_input("> ")
-    if req_msg == "/q":
-        client_socket.send(req_msg.encode())
-        break
-    else:
-        client_socket.send(req_msg.encode())
-
-client_socket.close()
+    play()
+    break
+client_socket.close()  # close connection
--- server_chat.py
+++ server_chat.py
@@ -1,32 +1,104 @@
 # source: class book: Computer Networking by Kurose and Ross
+# https://medium.com/byte-tales/the-classic-tic-tac-toe-game-in-python-3-1427c68b8874
+
 import sys
 from socket import *
+import random
+import time
 
-server_port = 4247  # establish port number
+server_board = {'7': ' ', '8': ' ', '9': ' ',
+                '4': ' ', '5': ' ', '6': ' ',
+                '1': ' ', '2': ' ', '3': ' '}
+
+move_list = []
+
+server_port = 4248  # establish port number
 server_socket = socket(AF_INET, SOCK_STREAM)  # create streaming socket
-server_socket.bind((gethostname(), server_port))  # bind the socket to host and port
+server_socket.bind(('', server_port))  # bind the socket to host and port
 
 
-def chat():
+def check_if_won():
+    if server_board['7'] == server_board['8'] == server_board['9'] != ' ':
+        return True
+    elif server_board['4'] == server_board['5'] == server_board['6'] != ' ':
+        return True
+    elif server_board['1'] == server_board['2'] == server_board['3'] != ' ':
+        return True
+    elif server_board['1'] == server_board['4'] == server_board['7'] != ' ':
+        return True
+    elif server_board['2'] == server_board['5'] == server_board['8'] != ' ':
+        return True
+    elif server_board['3'] == server_board['6'] == server_board['9'] != ' ':
+        return True
+    elif server_board['7'] == server_board['5'] == server_board['3'] != ' ':
+        return True
+    elif server_board['1'] == server_board['5'] == server_board['9'] != ' ':
+        return True
+    else:
+        return False
+
+
+def draw():
+    player_socket.send("Draw.")
+
+
+def play():
     counter = 0
-    while True:
-        data_a = "From server: msg " + str(counter) + "."
-        counter = counter + 1
-        connection_socket.send(data_a.encode()) # send encoded message
-        print(data_a)
-        rec_msg = connection_socket.recv(1024).decode()  # receive message
-        print(rec_msg)  # print received message
-        if rec_msg == "/q":
-            connection_socket.close()  # close connection
-            server_socket.close()
-            sys.exit()
+    game_over = False
+    player_socket.send("Welcome to tic tac toe. To quit during the game, enter /q.")
+    time.sleep(1)
+    player_socket.send("Server is o. You are x.")
+    time.sleep(1)
+    player_socket.send("* board key *")
+    time.sleep(1)
+
+    while True:  # game loop
+        while True:  # validate spot isn't already taken
+            r = random.randint(1, 9)
+            print("Temp server move: " + str(r))  # output for error checking
+            if r not in move_list:
+                server_board[str(r)] = 'o'  # add server move
+                move_list.append(r)
+                break
+        print("Server move: " + str(r))
+        player_socket.send("|" + str(r) + "| : Servers' move")
+        time.sleep(1)
+        game_over = check_if_won()
+        if game_over:
+            player_socket.send("~~ Game over. Server won. ~~")
+            break  # exit game loop
+        counter += 1
+        if counter >= 9:
+            draw()
+            break  # exit game loop
+        player_socket.send("# Enter 1-9")
+        rec_msg = player_socket.recv(1024).decode()  # receive message
+        if rec_msg == "/q":  # player wants to quit game
+            print(rec_msg)
+            break
         else:
-            continue
+            move_list.append(int(rec_msg))
+            q = str(rec_msg)
+            server_board[q] = 'x'
+            print("Client move: " + str(q))
+            game_over = check_if_won()
+            if game_over:
+                player_socket.send("~~ Game over. You won. ~~")
+                break
+            counter += 1
+            if counter >= 9:
+                draw()
+                break
 
 
 # server waits
 server_socket.listen(1)
 print("The server on port %d is listening." % server_port)
 while True:  # connection is established
-    connection_socket, address = server_socket.accept()  # accept outside connection
-    chat()
+    player_socket, address = server_socket.accept()  # accept outside connection
+    print("Player has joined game.")
+    play()
+    print("Exiting game...")
+    player_socket.close()  # close client connection
+    server_socket.close()  # close server
+    sys.exit()  # exit
